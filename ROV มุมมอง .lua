f63e4dd96a04f5e1d58a1a8a511ee8d5e615e8be
boomsl <85865092+boomsl@users.noreply.github.com>
2022-03-06 02:37:40 +0700

    Add files via upload

---
+++ ROV มุมมอง .lua
@@ -0,0 +1,63 @@
+
+
+print('Game installed:', gg.isPackageInstalled('com.blayzegames.iosfps'))
+
+
+
+
+
+function V1()
+Hack = gg.choice({ 
+	MR1.."มุมมอง ",
+	"ออก"}, nil,"sᴄʀɪᴘᴛ ᴘʀᴏ ʀᴏᴠ \n       ʏᴏᴜᴛᴜʙᴇ : ʙᴏᴏᴍsʟ ") 
+	if Hack == nil then else
+	if Hack == 1 then R1() end
+	if Hack == 2 then exit() end
+  end
+  BSLT = -1
+ end
+function exit()
+  print("╔═════════ 🧡 ═════════╗  ")
+print("               👑สร้างสคริปต์โดย 👑                ")
+print("                     👩‍💻ʙᴏᴏᴍsʟ 👩‍💻                     ")
+print("╚═════════ ♠ ═════════╝  ")
+gg.toast('𝓑𝓞𝓞𝓜𝓢𝓛')
+os.exit()
+end
+
+
+MR1 = "〘🔴〙"
+  function R1()
+    if MR1 == "〘🔴〙" then
+      gg.setRanges(gg.REGION_ANONYMOUS)
+      gg.searchNumber("-8.09801548e-19F;1.2000004768F;0.0F;-.25607532e-14F:13", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+      gg.refineNumber("1.20000004768F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+      revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+      gg.editAll("6F", gg.TYPE_FLOAT)
+      gg.clearResults()
+      gg.sleep(300)
+      gg.sleep(300)
+      gg.toast(" 𝓑𝓞𝓞𝓜𝓢𝓛 ")
+      MR1 = "〘🔵〙"
+    elseif MR1 == "〘🔵〙" then
+      gg.setRanges(gg.REGION_ANONYMOUS)
+      gg.searchNumber("-8.09801548e-19F;6F;0.0F;-.25607532e-14F:1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+      gg.refineNumber("6F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+      revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+      gg.editAll("1.20000004768F", gg.TYPE_FLOAT)
+      gg.clearResults()
+      gg.sleep(300)
+      gg.sleep(300)
+      gg.toast("𝓑𝓞𝓞𝓜𝓢𝓛")
+      MR1 = "〘🔴〙"
+    end--𝓑𝓞𝓞𝓜𝓢𝓛
+  end--𝓑𝓞𝓞𝓜𝓢𝓛
+  
+V1()
+while true do 
+  if gg.isVisible(true) then 
+    BSLT = 1 
+  gg.setVisible(false) 
+  end 
+    if BSLT == 1 then V1() end 
+end 
